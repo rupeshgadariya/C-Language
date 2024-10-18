fad6f1aa92a4ff6131db6b719f1fd528a06c28cb
Rupesh Kumar <gadariyarupesh@gmail.com>
2024-10-18 21:47:10 +0530

    Add files via upload

---
+++ ConvertF.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+    int main()
+{
+
+        float F,C;
+        printf("Enter the celsius = ");
+        scanf("%f",&C);
+       F=(9.0/5.0)*C+32;
+
+        printf("Convert celsius into Fahrenheit = %f",F);
+    
+return 0;
+}
