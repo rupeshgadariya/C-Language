8a23813c63350a1b67a9a239825e74a62fa53e29
Rupesh Kumar <gadariyarupesh@gmail.com>
2025-04-17 17:01:13 +0530

    Update Interest.c

--- Interest.c
+++ Interest.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 #include<conio.h>
  
-    int main()
+    int main() 
 {
     float P,R,T;
                    //Calculate the simple interest
     printf("Enter the principal value P:\n");
    scanf("%f",&P);
-   printf("Enter the principal value R:\n");
+   printf("Enter the princip outal value R:\n");
   scanf("%f",&R);
     printf("Enter the principal value T:\n");
   scanf("%f",&T);
