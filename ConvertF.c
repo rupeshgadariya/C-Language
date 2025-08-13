b8e5522586068e0e0771caca95365a302467f58a
Rupesh Kumar <gadariyarupesh@gmail.com>
2025-08-13 22:14:16 +0530

    Update ConvertF.c

--- ConvertF.c
+++ ConvertF.c
@@ -4,13 +4,14 @@
      int main() 
 {
    float F,C;
-        printf("Enter the celsius = ");
+         printf("Enter the celsius = ");
         scanf("%f",&C);  
            F=(9.0/5.0)*C+32;
         printf("Convert celsius into Fahrenheit = %f",F);
     
 return 0;
 }
+
 
 
 
